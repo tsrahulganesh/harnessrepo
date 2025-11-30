9ea26be3d9fae8081db7efccb77149f82612e615
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 15:09:52 +0530

    newfile

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,70 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class DemoTest {
-
-    @Test
-    void openGoogleTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        assertTrue(driver.getTitle().toLowerCase().contains("google"));
-        driver.quit();
-    }
-
-    @Test
-    void openAmazonTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.in");
-        assertTrue(driver.getTitle().toLowerCase().contains("amazon"));
-        driver.quit();
-    }
-
-    @Test
-    void openFlipkartTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.flipkart.com");
-        assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
-        driver.quit();
-    }
-}
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class DemoTest {
-
-    @Test
-    void openGoogleTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        assertTrue(driver.getTitle().toLowerCase().contains("google"));
-        driver.quit();
-    }
-
-    @Test
-    void openAmazonTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.in");
-        assertTrue(driver.getTitle().toLowerCase().contains("amazon"));
-        driver.quit();
-    }
-
-    @Test
-    void openFlipkartTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.flipkart.com");
-        assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
-        driver.quit();
-    }
-}
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -93,33 +29,6 @@ public class DemoTest {
         WebDriver driver = new EdgeDriver();
         driver.get("https://www.flipkart.com");
         assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
-        driver.quit();
-    }
-}
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class DemoTest {
-
-    @Test
-    void openGoogleTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        assertEquals("Google", driver.getTitle());
-        driver.quit();
-    }
-}
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class DemoTest {
-
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        System.out.println("Page title: " + driver.getTitle());
         driver.quit();
     }
 }
