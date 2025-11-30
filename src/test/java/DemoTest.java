b114ea0d5a1bd65d6cacc3f33270ba17ea7ddca4
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 14:56:12 +0530

    multiplebrowser

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,6 +1,40 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoTest {
+
+    @Test
+    void openGoogleChrome() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        assertTrue(driver.getTitle().toLowerCase().contains("google"));
+        driver.quit();
+    }
+
+    @Test
+    void openAmazonFirefox() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.amazon.in");
+        assertTrue(driver.getTitle().toLowerCase().contains("amazon"));
+        driver.quit();
+    }
+
+    @Test
+    void openFlipkartEdge() {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.flipkart.com");
+        assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
+        driver.quit();
+    }
+}
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoTest {
