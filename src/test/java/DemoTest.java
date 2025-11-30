9770ebf25e6c60f7d1dd0963abc11539ab46e4f4
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 15:44:19 +0530

    new changes

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,14 +1,24 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoTest {
 
+    private WebDriver getDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        return new ChromeDriver(options);
+    }
+
     @Test
     void openGoogleChrome() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = getDriver();
         driver.get("https://www.google.com");
         assertTrue(driver.getTitle().toLowerCase().contains("google"));
         driver.quit();
@@ -16,7 +26,7 @@ public class DemoTest {
 
     @Test
     void openAmazon() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = getDriver();
         driver.get("https://www.amazon.in");
         assertTrue(driver.getTitle().toLowerCase().contains("amazon"));
         driver.quit();
@@ -24,7 +34,7 @@ public class DemoTest {
 
     @Test
     void openFlipkart() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = getDriver();
         driver.get("https://www.flipkart.com");
         assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
         driver.quit();
