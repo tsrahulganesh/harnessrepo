214633edf45b200d303a5a3ee40b2410f3f82587
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 12:23:25 +0530

    Intial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UrlTest {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Page title: " + driver.getTitle());
+        driver.quit();
+    }
+}
+
