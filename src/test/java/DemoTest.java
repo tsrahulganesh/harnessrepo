2aac2fdd4ab18153c5a3904315b86d733a65af6a
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 13:52:59 +0530

    Fixed pom

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class UrlTest {
+public class DemoTest {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
