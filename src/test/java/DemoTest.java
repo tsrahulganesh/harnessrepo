af97b298adab33e7ce3eef82b5a0d82e834c50c5
Rahulganesh <tsrahulganesh@gmail.com>
2025-11-30 14:59:28 +0530

    3case-1browser

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,6 +1,38 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoTest {
+
+    @Test
+    void openGoogleTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        assertTrue(driver.getTitle().toLowerCase().contains("google"));
+        driver.quit();
+    }
+
+    @Test
+    void openAmazonTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in");
+        assertTrue(driver.getTitle().toLowerCase().contains("amazon"));
+        driver.quit();
+    }
+
+    @Test
+    void openFlipkartTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com");
+        assertTrue(driver.getTitle().toLowerCase().contains("flipkart"));
+        driver.quit();
+    }
+}
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
